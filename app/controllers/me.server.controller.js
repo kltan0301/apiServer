0903943bdebe7c69c1172f70546516062e64911d
Kai Lin Tan <kailin.tan.2010@gmail.com>
2016-08-16 17:43:33 +0800

    Update json

--- app/controllers/me.server.controller.js
+++ app/controllers/me.server.controller.js
@@ -2,7 +2,7 @@ module.exports = {
   index: function(req, res){
     res.json({
       profilePic:'http://www.playgamesarcade.com/images/avatars/calvin_avatar.jpg',
-      name: 'Tan Kai Lin',
+      name: 'Kai Lin',
       title: 'Game Designer',
       location: 'Singapore'
     });
